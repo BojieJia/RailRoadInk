3770ddceb40b5a6532ce35df15cb1c312c62c06f
jay <u6730978@anu.edu.au>
2019-05-07 01:25:58 +1000

    test in task6

--- tests/comp1110/ass2/TaskTestSix.java
+++ tests/comp1110/ass2/TaskTestSix.java
@@ -1,29 +1,20 @@
 package comp1110.ass2;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Timeout;
 
 import java.util.Random;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+//Authored by Bojie 
 
-//@Test
-//public void testGood() {
-  //      Random r = new Random();
-
-    //    for (int i = 0; i < SampleGames.COMPLETED_GAMES.length; i++) {
-      //  String p = SampleGames.COMPLETED_GAMES[i];
-       // for (int j = 5; j < p.length(); j += 5) {
-       // String test = p.substring(0, j);
-       // assertTrue("Placement sequence '" + test + "' is valid, but failed ", RailroadInk.isValidPlacementSequence(test));
-        //}
-       // }
-        //}
-public class TaskSixTest {
+public class TaskTestSix {
 
     @Test
     public void ifIsNotCover(){
-        for(String test:notCoverPlacement){
-            assertTrue("Placement sequence '" + test + "' is cover, but passed ", RailroadInk.notCover(test));
+        for(String test:CoverPlacement){
+            assertFalse("Placement sequence '" + test + "' is cover, but passed ", RailroadInk.notCover(test));
         }
     }
 
@@ -32,24 +23,31 @@ public class TaskSixTest {
 
     public void ifIsValidExit(){
         for(String test:notValidExit){
-            assertTrue("Placement sequence '" + test + "' is valid, but failed ", RailroadInk.isExit(test));
+            assertFalse("Placement sequence '" + test + "' is valid, but failed ", RailroadInk.isExit(test));
         }
     }
+
 
     @Test
     public void ifIsValidNeighbor(){
         for(String test:inValidNeighbor){
-            assertTrue("Placement sequence '" + test + "' is valid, but failed ", RailroadInk.isNeighbor(test));
+            assertFalse("Placement sequence '" + test + "' is valid, but failed ", RailroadInk.isNeighbor(test));
+        }
+    }
+
+    @Test
+    public void isGood(){
+        for(String test:goodString){
+            assertTrue("Placement sequence '" + test + "' is valid, but failed ", RailroadInk.isValidPlacementSequence(test));
         }
     }
 
 
 
-    static final String[] notCoverPlacement={
-            "A3B10A2B20A3B30A4B40S1C10S2C26",
-            "B1D02B1D32B1F22B0G00",
+    static final String[] CoverPlacement={
+           "A1F10A2F16A3F17A4F22S1G27",
             "A3B10S3B10B2B12"
-        };
+    };
     static final String[] notValidExit={
 
             "A3D61A3D53B0C52A0B52A2B63A4D41B0E60A0F61A3D31A3D23A2G30B0F34A3E32A1B01B2B10A0A63A1G41S2D10B2A10B0A30A4E21A3C31",
@@ -59,6 +57,14 @@ public class TaskSixTest {
     static final String[] inValidNeighbor={
             "A3A52A2B61A0C60A4A41S5B34B0D32A2C50A3D12B2B10A0G00A4D01B1A27S3B20A4C10A1D50A0F23B2G25A3E30A4E41",
             "A2A30A0A43A3A50B2B50A4C50A3B20B0G12A2B33A4E50A2B21B1G45B2F41A3F33A1E40A3E32A3E27S0E12B1D17A1B61"
-    }
+    };
+
+    static final String[] goodString={
+            "A4A16B2B14S2C13S5D12S3D25",
+            "A1A30A1B32S3C30A20D3A10E3A1F30A1G30"
+    };
+
+
+
 
 }
